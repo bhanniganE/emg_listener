b4e463833c68b62588b102bfd06ee8111d03c42b
bhanniganE <95034537+bhanniganE@users.noreply.github.com>
2023-02-17 09:00:45 -0700

    Update emg_listener.py

--- emg_listener.py
+++ emg_listener.py
@@ -12,11 +12,12 @@ from dotenv import load_dotenv
 from os.path import exists
 
 from utils.messages import receive_messages, dispatchResource, dispatchBilling
-    
+
+
 '''
    interrupt handlers
 '''
-   
+
 def sigint_handler(sig, frame):
     print('exiting..')
     sys.exit(0)
@@ -24,13 +25,13 @@ def sigint_handler(sig, frame):
 
 '''
    main loop
-'''   
-    
+'''
+
 async def main():
     if not exists(".env"):
         print("error: .env file not found. exiting.")
         sys.exit(1)
-    
+
     try:
         load_dotenv()
     except AttributeError:
@@ -38,25 +39,26 @@ async def main():
         sys.exit(1)
 
     print('launching listener.. ctrl+C to exit.')
-    
+
     while (True):
         for msg in await receive_messages():
             # if billing details are present, call the billing dispatcher  
             try:
-                if "LineDetails" in msg["Body"].keys():
+                if 'LineDetails' in msg["Body"]:
                     dispatchBilling(msg)
+                    continue
             except KeyError:
                 print('billing fail..')
                 continue
                 #pass
-                
+
             # call the event handler based on the resource type
             try:
                 dispatchResource(msg)
             except:
                 print("dispatchResource()")
-                
+
 
 if __name__ == "__main__":
     signal.signal( signal.SIGINT, sigint_handler )
-    asyncio.run( main() ) 
+    asyncio.run( main() )
